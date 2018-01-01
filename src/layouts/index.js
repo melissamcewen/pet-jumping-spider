a50d8b3e1b2ae18b6d7fae1f699ccf40289e7226
Melissa Mcewen <melissa@equivoice.com>
2017-12-31 19:33:48 -0500

    using local bootstrap intesad

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -5,6 +5,7 @@ import Helmet from 'react-helmet'
 import Nav from '../components/nav'
 import ReactGA from 'react-ga'
 import { withPrefix } from 'gatsby-link'
+import '../../node_modules/bootstrap/dist/css/bootstrap.css';
 
 
 const Header = () => (
@@ -61,7 +62,7 @@ export default class TemplateWrapper extends React.Component {
             }
           ]}
         >
-          <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/css/bootstrap.min.css" integrity="sha384-PsH8R72JQ3SOdhVi3uxftmaW6Vc51MKb0q5P2rRUpPvrszuE4W1povHYgTpBfshb" crossorigin="anonymous" />
+ 
       </Helmet>
         <Header />
         <div
